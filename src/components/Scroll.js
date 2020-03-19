5a120a0b4441a2f8bcc63872e8692f6e556e2aa8
Shashank  Shahare <srshahare22@gmail.com>
2020-03-19 06:58:41 -0700

    new commit added extra features

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return(
-        <div style={{overflowY: 'scroll', border: '1px solid black', height: '70.6vh'}}>
+        <div style={{overflowY: 'scroll', border: '1px solid black', height: '70vh'}}>
             { props.children }
         </div>
     );
