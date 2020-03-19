d3044047ccd3f1066ffb1769cec91f5defc1cfd0
Shashank  Shahare <srshahare22@gmail.com>
2020-03-19 01:49:57 -0700

    This is first commit added files

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import './App.css'
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll'
+
+class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(user => this.setState({ robots: user }));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+      
+    }
+
+    render(){
+        const { robots, searchfield } = this.state;
+        const filterRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        return !robots.length ? 
+        <h1>Loading...</h1> :  
+        (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filterRobots}/>
+                </Scroll>
+            </div>   
+        )      
+    }
+}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,87 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Shashank Shahare",
+        username: "Shashank",
+        email: "srshahare22@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Ramesh Shahare",
+        username: "Ramesh",
+        email: "ramesh@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Shubham Shahare",
+        username: "Shubham",
+        email: "shubham@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Rutuja Shahare",
+        username: "Rutuja",
+        email: "rutuja@gmail.com"
+    },
+    {
+        id: 5,
+        name: "Rajani Shahare",
+        username: "Rajani",
+        email: "rajani@gmail.com"
+    },
+    {
+        id: 6,
+        name: "Nishant Waghmare",
+        username: "Nishant",
+        email: "nishant@gmail.com"
+    },
+    {
+        id: 7,
+        name: "Shivam Tarone",
+        username: "Shivam",
+        email: "shivam@gmail.com"
+    },
+    {
+        id: 8,
+        name: "Shashank Shahare",
+        username: "Shashank",
+        email: "srshahare22@gmail.com"
+    },
+    {
+        id: 9,
+        name: "Ramesh Shahare",
+        username: "Ramesh",
+        email: "ramesh@gmail.com"
+    },
+    {
+        id: 10,
+        name: "Shubham Shahare",
+        username: "Shubham",
+        email: "shubham@gmail.com"
+    },
+    {
+        id: 11,
+        name: "Rutuja Shahare",
+        username: "Rutuja",
+        email: "rutuja@gmail.com"
+    },
+    {
+        id: 12,
+        name: "Rajani Shahare",
+        username: "Rajani",
+        email: "rajani@gmail.com"
+    },
+    {
+        id: 13,
+        name: "Nishant Waghmare",
+        username: "Nishant",
+        email: "nishant@gmail.com"
+    },
+    {
+        id: 14,
+        name: "Shivam Tarone",
+        username: "Shivam",
+        email: "shivam@gmail.com"
+    },
+   
+]
